7adc9b0319bf12449f943839fd79b7b11fb1b01f
Peyman <still.peyman@gmail.com>
2025-05-15 15:04:08 +0200

    API endpoint created to add a new blog post.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,8 +10,28 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
-def get_posts():
+def validate_post_data(data):
+    if "title" not in data or "content" not in data:
+        return False
+    return True
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
+def handle_posts():
+    if request.method == 'POST':
+        # Get the new post data from the client
+        new_post = request.get_json()
+        if not validate_post_data(new_post):
+            return jsonify({"error": "Invalid post data. Must include a title and content"}), 400
+        
+        # Generate a new ID for the post
+        new_id = max(post['id'] for post in POSTS) + 1
+        new_post['id'] = new_id
+        
+        POSTS.append(new_post)
+        return jsonify(new_post), 201
+
+    # GET request to return all posts
     return jsonify(POSTS)
 
 
